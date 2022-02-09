9ea6e859b90502b91bd43ddb89892042be87b39c
Primarter <alec.ferrari@epitech.eu>
2022-02-09 17:48:31 +0100

    feat(master): nix setup and working

--- default.nix
+++ default.nix
@@ -1,27 +1,21 @@
 let
   pkgs = import (builtins.fetchGit {
       url = https://github.com/NixOS/nixpkgs;
-      rev = "c7655fa3087cec120b13425f3e16bf378065fd8c";
+      rev = "521e4d7d13b09bc0a21976b9d19abd197d4e3b1e";
     }) {};
 
-  hpkgs = pkgs.haskell.packages.ghc8107;
+  hpkgs = pkgs.haskell.packages.ghc8107.override {
+    overrides = self: super: {
+      llvm-hs = super.llvm-hs.override { llvm-config = pkgs.llvm_9; };
+    };
+  };
 
   koak = hpkgs.callCabal2nix "koak" ./. {};
-  koak2-static = pkgs.haskell.lib.justStaticExecutables koak;
-  koak-static = pkgs.haskell.lib.overrideCabal koak (old: {
-    enableSharedExecutables = false;
-    enableSharedLibraries = false;
-    configureFlags = [
-      "--ghc-option=-optl=-static"
-      "--ghc-option=-optl=-L${pkgs.gmp6.override { withStatic = true; }}/lib"
-      "--ghc-option=-optl=-L${pkgs.zlib.static}/lib"
-      "--ghc-option=-optl=-L${pkgs.glibc.static}/lib"
-    ];
-  });
+  koak-static = pkgs.haskell.lib.justStaticExecutables koak;
 in
 
 if pkgs.lib.inNixShell then hpkgs.shellFor {
   packages = p: with hpkgs; [ koak p.haskell-language-server ];
-  buildInputs = [ pkgs.stack ];
+  buildInputs = [ pkgs.stack pkgs.llvm_9 ];
   withHoogle = true;
 } else koak
--- src/Types/AST.hs
+++ src/Types/AST.hs
@@ -35,18 +35,18 @@ instance Monoid Env where mempty = Env mempty
 instance Show Env where show = show . M.toList . getEnv
 
 
-data Expr
+data ASTF rec
   = Float Double
   | Int Integer
-  | BinOp Op Expr Expr
+  | BinOp Op rec rec
   | Var VarName
-  | Call VarName [Expr]
-  | Function VarName [Expr] Expr
+  | Call VarName [rec]
+  | Function VarName [rec] rec
   -- | Function { fnName :: VarName
   --            , fnArgs :: ![VarName]
   --            , fnBody :: !r
   --            , fnEnv  :: Env }
-  | Extern VarName [Expr]
+  | Extern VarName [rec]
   deriving (Eq, Ord, Show)
 
 data Op = Plus
@@ -56,19 +56,19 @@ data Op = Plus
   deriving (Eq, Ord, Show)
 
 -- | whole AST definition
-data ASTF r = Atom !VarName
-            -- | Int  !Integer
-            | Bool !Bool
-            -- | Real !Rational
-            | Str  !String
-            | List ![r]
-            | DottedList ![r] !r
-            -- lambda grabs closure from current env (a la python)
-            | Builtin (Func Interp)
-            | Lambda { fnArgs :: ![VarName]
-                     , fnBody :: !r
-                     , fnEnv  :: Env }
-            deriving (Show, Eq)
+-- data ASTF r = Atom !VarName
+--             -- | Int  !Integer
+--             | Bool !Bool
+--             -- | Real !Rational
+--             | Str  !String
+--             | List ![r]
+--             | DottedList ![r] !r
+--             -- lambda grabs closure from current env (a la python)
+--             | Builtin (Func Interp)
+--             | Lambda { fnArgs :: ![VarName]
+--                      , fnBody :: !r
+--                      , fnEnv  :: Env }
+--             deriving (Show, Eq)
 
 newtype Func m = Func { getFn :: [AST'] -> m AST' }
 
@@ -78,49 +78,24 @@ instance   Eq (Func m) where _ == _ = True
 
 -- Instances {{{
 instance Functor ASTF where
-  fmap _ (Atom a) = Atom a
-  fmap _  (Str a) = Str  a
-  fmap _ (Bool a) = Bool a
+  -- fmap _  (Str a) = Str  a
   -- fmap _  (Int a) = Int a
-  fmap f (List r) = List $ f <$> r
-  fmap f (DottedList r h) = DottedList (f <$> r) (f h)
-  fmap f (Lambda p b c)   = Lambda p (f b) c
-  fmap _ (Builtin p) = Builtin p
   -- fmap _ (Real a) = Real a
 
 instance Foldable ASTF where
-  foldMap _ (Atom _) = mempty
-  foldMap _  (Str _) = mempty
-  foldMap _ (Bool _) = mempty
+  -- foldMap _  (Str _) = mempty
   -- foldMap _  (Int _) = mempty
-  foldMap f (List r) = foldMap f r
-  foldMap f (DottedList r h) = foldMap f r <> f h
-  foldMap f (Lambda _ b _)   = f b
-  foldMap _ (Builtin _) = mempty
   -- foldMap _ (Real _) = mempty
 
 instance Traversable ASTF where
-  traverse _ (Atom a) = pure $ Atom a
-  traverse _  (Str a) = pure $ Str a
-  traverse _ (Bool a) = pure $ Bool a
+  -- traverse _  (Str a) = pure $ Str a
   -- traverse _  (Int a) = pure $ Int a
-  traverse f (List r) = List <$> traverse f r
-  traverse f (DottedList xs x) = DottedList <$> traverse f xs <*> f x
-  traverse f (Lambda ps b ctx) = flip (Lambda ps) ctx <$> f b
-  traverse _ (Builtin p) = pure $ Builtin p
   -- traverse _ (Real a) = pure $ Real a
 
 instance Show1 ASTF where
-  liftShowsPrec _ _ _ (Atom a) = showString a
   -- liftShowsPrec _ _ _  (Int s) = shows s
-  liftShowsPrec _ _ _  (Str s) = showChar '"' . showString (concatMap
-    (\a -> if a == '"' then "\\\"" else [a]) s) . showChar '"'
-  liftShowsPrec _ _ _ (Bool a) = showString $ if a then "#t" else "#f"
-  liftShowsPrec f _ p (List a) = showChar '(' . showList' (showChar ')') f p a
-  liftShowsPrec pf _ p (DottedList a h) = showChar '(' . showList'
-    (showString " . " . pf p h . showChar ')') pf p a
-  liftShowsPrec _ _ _ Lambda{} = showString "#<procedure>"
-  liftShowsPrec _ _ _ Builtin{} = showString "#<primitive>"
+  -- liftShowsPrec _ _ _  (Str s) = showChar '"' . showString (concatMap
+  --   (\a -> if a == '"' then "\\\"" else [a]) s) . showChar '"'
   -- liftShowsPrec _ _ _ (Real s) = shows (fromRational s :: Double)
 
 showList' :: ShowS -> (a -> b -> ShowS) -> a -> [b] -> ShowS
@@ -128,15 +103,8 @@ showList' end pf p =
   fmap (pf p) >>> L.intersperse (showChar ' ') >>> foldr (.) end
 
 instance Eq1 ASTF where
-  liftEq _ (Atom a) (Atom b) = a == b
   -- liftEq _  (Int a)  (Int b) = a == b
-  liftEq _  (Str a)  (Str b) = a == b
-  liftEq _ (Bool a) (Bool b) = a == b
-  liftEq e (List a) (List b) = and $ zipWith e a b
-  liftEq e (DottedList as a) (DottedList bs b) = and $ zipWith e (a:as) (b:bs)
-  liftEq e (Lambda vs body env) (Lambda vs' body' env') =
-    vs == vs' && e body body' && env == env'
-  liftEq _ (Builtin a) (Builtin b) = a == b
+  -- liftEq _  (Str a)  (Str b) = a == b
   liftEq _ _ _ = False
 -- }}}
 
@@ -154,32 +122,10 @@ newtype ASTWithHist =
 -- node :: f (Cofree f Pos) -> Cofree f Pos
 -- node a = nPos C.:< a
 
-list :: [AST'] -> AST'
-list = Fix . List
-
-atom :: VarName -> AST'
-atom = Fix . Atom
-
-str :: String -> AST'
-str = Fix . Str
+-- str :: String -> AST'
+-- str = Fix . Str
 
 -- int :: Integer -> AST'
 -- int = Fix . Int
-
-bool :: Bool -> AST'
-bool = Fix . Bool
-
-mkBuiltin :: Func Interp -> AST'
-mkBuiltin = Fix . Builtin
-
-mkQuote :: AST' -> AST'
--- mkQuote (Fix(List as)) = list $ atom "quote" :as
-mkQuote a = list [atom "quote", a]
-
-dlist :: [AST'] -> AST' -> AST'
-dlist as = Fix . DottedList as
-
-func :: [VarName] -> AST' -> Env -> AST'
-func ps body ctx = Fix $ Lambda ps body ctx
 
 -- }}}
